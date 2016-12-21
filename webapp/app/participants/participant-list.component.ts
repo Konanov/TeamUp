bce657c8c28c2ab8210e30ffabaaab5629cc21a0
konanov <konanov@imp-m.ru>
2016-12-21 18:12:31 +0300

    Participant's card routing in modal window.

--- webapp/app/participants/participant-list.component.ts
+++ webapp/app/participants/participant-list.component.ts
@@ -9,11 +9,11 @@ import {Observable} from "rxjs";
   selector: 'participants-list',
   template: `
   <div>
-  <button *ngFor="let participant of participants" class="btn btn-primary" type="button" (click)="onSelect(participant)">
+  <button *ngFor="let participant of participants" class="btn btn-primary" type="button" data-toggle="modal" data-target="#myModal" (click)="onSelect(participant)">
   <div class="row"><img class="img-circle" style="height: 25px; width: 25px" src="{{avatarUrl}}{{participant.id}}"></div>
   <div class="row"><div class="label">{{participant.name}} {{participant.surname}}</div></div>
   </button>
-  </div>`,
+</div>`,
 
   providers: [ParticipantsService]
 })
--- webapp/app/participants/participant.component.ts
+++ webapp/app/participants/participant.component.ts
@@ -6,17 +6,31 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'participant-details',
   template: `
-  <div class="card card-block" *ngIf="participant">
-  <h4 class="card-title">{{participant.name}} {{participant.surname}}</h4>
-  <p class="card-text">{{participant.currentTask?.description}}</p>
-  <a href="#" class="card-link">Card link</a>
-  <a href="#" class="card-link">Another link</a>
-</div>`,
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <div class="modal-content" *ngIf="participant">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title"><img class="img-circle" style="height: 40px; width: 40px" src="{{avatarUrl}}{{id}}"> {{participant.name}} {{participant.surname}}</h4>
+      </div>
+      <div class="modal-body">
+        <p>{{participant.currentTask?.description}}</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>`,
 
   providers: [ParticipantsService],
 })
 
 export class Participant {
+
+  private id: string;
+
+  private avatarUrl = "http://localhost:8080/participants/avatar/";
 
   private sub:any;
   private participant: Participant;
@@ -30,6 +44,8 @@ export class Participant {
     this.sub = this.route.params.subscribe(params => {
 
       let id = params['id'];
+
+      this.id = id;
 
       this.service.getParticipantDetails(id).subscribe(participant => this.participant = participant);
     });
